8c4ec7f09755e26d7f43802114959eff15c7e9d8
Seva Zaikov <seva.zaikov@gmail.com>
2025-06-12 10:22:26 -0700

    Extract images list logic into a separate VM (#12)

---
+++ Piktosaur/ViewModels/ImagesListVM.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+using Piktosaur.Models;
+using Piktosaur.Services;
+
+namespace Piktosaur.ViewModels
+{
+    public class ImagesListVM : BaseViewModel
+    {
+        private AppStateVM appStateVM;
+
+        private List<FolderWithImages> folders = new();
+
+        private CancellationTokenSource? cancellationTokenSource;
+
+        private bool hasSelectedImage = false;
+
+        private bool loading = false;
+
+        public bool Loading
+        {
+            get => loading;
+            private set => SetProperty(ref loading, value);
+        }
+
+        public ImagesListVM(AppStateVM appStateVM)
+        {
+            this.appStateVM = appStateVM;
+        }
+
+        public async Task<List<FolderWithImages>> LoadImages()
+        {
+            var currentQuery = appStateVM.SelectedQuery;
+            var result = Search.GetImages(currentQuery.Folders[0]);
+            var images = result.Results;
+
+            List<Task> thumbnailTasks = [];
+
+            cancellationTokenSource = new CancellationTokenSource();
+
+            foreach (var image in images.Take(10))
+            {
+                thumbnailTasks.Add(image.GenerateThumbnail(cancellationTokenSource.Token));
+            }
+
+            SelectFirstImage(images);
+
+            Loading = true;
+
+            await Task.WhenAll(thumbnailTasks);
+
+            cancellationTokenSource.Token.ThrowIfCancellationRequested();
+
+            Loading = false;
+
+            var folderName = System.IO.Path.GetFileName(result.DirectoryPath);
+            var folderWithImages = new FolderWithImages(folderName, images);
+
+            folders.Add(folderWithImages);
+
+            HandleSubdirectories(result.SubdirectoriesImagesData, folders);
+
+            return folders;
+        }
+
+        private void HandleSubdirectories(List<ImagesData> imagesDataList, List<FolderWithImages> folders)
+        {
+            foreach (var imagesData in imagesDataList)
+            {
+                var folderName = System.IO.Path.GetFileName(imagesData.DirectoryPath);
+                var folderWithImages = new FolderWithImages(folderName, imagesData.Results);
+
+                SelectFirstImage(imagesData.Results);
+
+                folders.Add(folderWithImages);
+
+                HandleSubdirectories(imagesData.SubdirectoriesImagesData, folders);
+            }
+        }
+
+        private void SelectFirstImage(List<ImageResult> images)
+        {
+            if (hasSelectedImage == true) return;
+
+            if (images.Count > 0)
+            {
+                appStateVM.SelectImage(images[0].Path);
+                hasSelectedImage = true;
+            }
+        }
+
+        public void ClearFoldersData()
+        {
+            cancellationTokenSource?.Cancel();
+            foreach (var folder in folders)
+            {
+                folder?.Dispose();
+            }
+        }
+    }
+}
